f98cc54189dbfe91830f4ec93113da5bce8a961e
rick <rick@99205bd7-9fcc-e011-96ac-0030489379b8>
2011-08-23 20:08:06 +0000

    - Add Gibbed.DeusEx3.FileFormats, Gibbed.DeusEx3.Unpack, Test, RebuildFileLists.

---
+++ trunk/Gibbed.DeusEx3.FileFormats/StringHelpers.cs
@@ -0,0 +1,49 @@
+﻿/* Copyright (c) 2011 Rick (rick 'at' gibbed 'dot' us)
+ * 
+ * This software is provided 'as-is', without any express or implied
+ * warranty. In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ * 
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ * 
+ * 1. The origin of this software must not be misrepresented; you must not
+ *    claim that you wrote the original software. If you use this software
+ *    in a product, an acknowledgment in the product documentation would
+ *    be appreciated but is not required.
+ * 
+ * 2. Altered source versions must be plainly marked as such, and must not
+ *    be misrepresented as being the original software.
+ * 
+ * 3. This notice may not be removed or altered from any source
+ *    distribution.
+ */
+
+namespace Gibbed.DeusEx3.FileFormats
+{
+    public static class StringHelpers
+    {
+        public static uint HashFileName(this string input)
+        {
+            uint hash = 0xFFFFFFFFu;
+            for (int i = 0; i < input.Length; i++)
+            {
+                hash ^= (uint)input[i] << 24;
+
+                for (int j = 0; j < 8; j++)
+                {
+                    if ((hash & 0x80000000) != 0)
+                    {
+                        hash = (hash << 1) ^ 0x04C11DB7u;
+                    }
+                    else
+                    {
+                        hash <<= 1;
+                    }
+                }
+            }
+            return ~hash;
+        }
+    }
+}
---
+++ trunk/Gibbed.DeusEx3.Unpack/Program.cs
@@ -0,0 +1,278 @@
+﻿/* Copyright (c) 2011 Rick (rick 'at' gibbed 'dot' us)
+ * 
+ * This software is provided 'as-is', without any express or implied
+ * warranty. In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ * 
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ * 
+ * 1. The origin of this software must not be misrepresented; you must not
+ *    claim that you wrote the original software. If you use this software
+ *    in a product, an acknowledgment in the product documentation would
+ *    be appreciated but is not required.
+ * 
+ * 2. Altered source versions must be plainly marked as such, and must not
+ *    be misrepresented as being the original software.
+ * 
+ * 3. This notice may not be removed or altered from any source
+ *    distribution.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Gibbed.DeusEx3.FileFormats;
+using Big = Gibbed.DeusEx3.FileFormats.Big;
+using Gibbed.IO;
+using System.Xml;
+using NDesk.Options;
+
+namespace Gibbed.DeusEx3.Unpack
+{
+    internal class Program
+    {
+        private static string GetExecutableName()
+        {
+            return Path.GetFileName(System.Reflection.Assembly.GetExecutingAssembly().CodeBase);
+        }
+
+        public static void Main(string[] args)
+        {
+            bool showHelp = false;
+            bool extractUnknowns = true;
+            bool overwriteFiles = false;
+            bool verbose = true;
+
+            var options = new OptionSet()
+            {
+                {
+                    "o|overwrite",
+                    "overwrite existing files",
+                    v => overwriteFiles = v != null
+                },
+                {
+                    "nu|no-unknowns",
+                    "don't extract unknown files",
+                    v => extractUnknowns = v == null
+                },
+                {
+                    "v|verbose",
+                    "be verbose",
+                    v => verbose = v != null
+                },
+                {
+                    "h|help",
+                    "show this message and exit", 
+                    v => showHelp = v != null
+                },
+            };
+
+            List<string> extras;
+
+            try
+            {
+                extras = options.Parse(args);
+            }
+            catch (OptionException e)
+            {
+                Console.Write("{0}: ", GetExecutableName());
+                Console.WriteLine(e.Message);
+                Console.WriteLine("Try `{0} --help' for more information.", GetExecutableName());
+                return;
+            }
+
+            if (extras.Count < 1 ||
+                extras.Count > 2 ||
+                showHelp == true ||
+                Path.GetExtension(extras[0]) != ".000")
+            {
+                Console.WriteLine("Usage: {0} [OPTIONS]+ input_file.000 [output_dir]", GetExecutableName());
+                Console.WriteLine();
+                Console.WriteLine("Options:");
+                options.WriteOptionDescriptions(Console.Out);
+                return;
+            }
+
+            string inputPath = extras[0];
+            string outputPath = extras.Count > 1 ? extras[1] : Path.ChangeExtension(inputPath, null) + "_unpack";
+            string datPath;
+
+            var manager = ProjectData.Manager.Load();
+            if (manager.ActiveProject == null)
+            {
+                Console.WriteLine("Warning: no active project loaded.");
+            }
+
+            var big = new BigFile();
+            using (var input = File.OpenRead(inputPath))
+            {
+                big.Deserialize(input);
+            }
+
+            var hashes = manager.LoadLists(
+                "*.filelist",
+                s => s.HashFileName(),
+                s => s.ToLowerInvariant());
+
+            Directory.CreateDirectory(outputPath);
+
+            var settings = new XmlWriterSettings();
+            settings.Indent = true;
+
+            using (var xml = XmlWriter.Create(
+                Path.Combine(outputPath, "bigfile.xml"), settings))
+            {
+                xml.WriteStartDocument();
+                xml.WriteStartElement("files");
+                xml.WriteAttributeString("endian", big.LittleEndian == true ? "little" : "big");
+                xml.WriteAttributeString("basepath", big.BasePath);
+
+                Stream data = null;
+                uint? currentBigFile = null;
+                uint? lastLocale = null;
+                var maxBlocksPerFile = big.FileAlignment / 2048;
+                {
+                    long current = 0;
+                    long total = big.Entries.Count;
+
+                    foreach (var entry in big.Entries.OrderBy(e => e.Offset))
+                    {
+                        current++;
+
+                        string name = hashes[entry.NameHash];
+                        if (name == null)
+                        {
+                            if (extractUnknowns == false)
+                            {
+                                continue;
+                            }
+
+                            string extension;
+                            // detect type
+                            {
+                                var guess = new byte[64];
+                                int read = 0;
+
+                                if (entry.Size > 0)
+                                {
+                                    var entryBigFile = entry.Offset / maxBlocksPerFile;
+                                    var entryOffset = (entry.Offset % maxBlocksPerFile) * 2048;
+
+                                    if (currentBigFile.HasValue == false ||
+                                        currentBigFile.Value != entryBigFile)
+                                    {
+                                        if (data != null)
+                                        {
+                                            data.Close();
+                                            data = null;
+                                        }
+
+                                        currentBigFile = entryBigFile;
+                                        data = File.OpenRead(Path.ChangeExtension(inputPath,
+                                            "." + currentBigFile.ToString().PadLeft(3, '0')));
+                                    }
+
+                                    data.Seek(entryOffset, SeekOrigin.Begin);
+                                    read = data.Read(guess, 0, (int)Math.Min(
+                                        entry.Size, guess.Length));
+                                }
+
+                                extension = FileExtensions.Detect(guess, Math.Min(guess.Length, read));
+                            }
+
+                            name = entry.NameHash.ToString("X8");
+                            name = Path.ChangeExtension(name, "." + extension);
+                            name = Path.Combine(extension, name);
+                            name = Path.Combine("__UNKNOWN", name);
+                        }
+                        else
+                        {
+                            name = name.Replace("/", "\\");
+                            if (name.StartsWith("\\") == true)
+                            {
+                                name = name.Substring(1);
+                            }
+                        }
+
+                        if (entry.Locale == 0xFFFFFFFF)
+                        {
+                            name = Path.Combine("default", name);
+                        }
+                        else
+                        {
+                            name = Path.Combine(entry.Locale.ToString("X8"), name);
+                        }
+
+                        var entryPath = Path.Combine(outputPath, name);
+                        Directory.CreateDirectory(Path.GetDirectoryName(entryPath));
+
+                        if (lastLocale.HasValue == false ||
+                            lastLocale.Value != entry.Locale)
+                        {
+                            xml.WriteComment(string.Format(" {0} = {1} ",
+                                entry.Locale.ToString("X8"),
+                                ((Big.Locale)entry.Locale)));
+                            lastLocale = entry.Locale;
+                        }
+
+                        xml.WriteStartElement("entry");
+                        xml.WriteAttributeString("hash", entry.NameHash.ToString("X8"));
+                        xml.WriteAttributeString("locale", entry.Locale.ToString("X8"));
+                        xml.WriteValue(name);
+                        xml.WriteEndElement();
+
+                        if (overwriteFiles == false &&
+                            File.Exists(entryPath) == true)
+                        {
+                            continue;
+                        }
+
+                        if (verbose == true)
+                        {
+                            Console.WriteLine("[{0}/{1}] {2}",
+                                current, total, name);
+                        }
+
+                        using (var output = File.Create(entryPath))
+                        {
+                            if (entry.Size > 0)
+                            {
+                                var entryBigFile = entry.Offset / maxBlocksPerFile;
+                                var entryOffset = (entry.Offset % maxBlocksPerFile) * 2048;
+
+                                if (currentBigFile.HasValue == false ||
+                                    currentBigFile.Value != entryBigFile)
+                                {
+                                    if (data != null)
+                                    {
+                                        data.Close();
+                                        data = null;
+                                    }
+
+                                    currentBigFile = entryBigFile;
+                                    data = File.OpenRead(Path.ChangeExtension(inputPath,
+                                        "." + currentBigFile.ToString().PadLeft(3, '0')));
+                                }
+
+                                data.Seek(entryOffset, SeekOrigin.Begin);
+                                output.WriteFromStream(data, entry.Size);
+                            }
+                        }
+                    }
+                }
+
+                if (data != null)
+                {
+                    data.Close();
+                }
+
+                xml.WriteEndElement();
+                xml.WriteEndDocument();
+                xml.Flush();
+            }
+        }
+    }
+}
