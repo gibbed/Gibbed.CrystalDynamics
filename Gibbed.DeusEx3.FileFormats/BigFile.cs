7004d2584bbdac29f3b919f2733da563d377d5c4
Rick Gibbed <gibbed@users.noreply.github.com>
2011-08-23 20:08:06 +0000

    - Add Gibbed.DeusEx3.FileFormats, Gibbed.DeusEx3.Unpack, Test, RebuildFileLists.

---
+++ Gibbed.DeusEx3.FileFormats/BigFile.cs
@@ -0,0 +1,118 @@
+﻿/* Copyright (c) 2011 Rick (rick 'at' gibbed 'dot' us)
+ * 
+ * This software is provided 'as-is', without any express or implied
+ * warranty. In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ * 
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ * 
+ * 1. The origin of this software must not be misrepresented; you must not
+ *    claim that you wrote the original software. If you use this software
+ *    in a product, an acknowledgment in the product documentation would
+ *    be appreciated but is not required.
+ * 
+ * 2. Altered source versions must be plainly marked as such, and must not
+ *    be misrepresented as being the original software.
+ * 
+ * 3. This notice may not be removed or altered from any source
+ *    distribution.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Gibbed.IO;
+
+namespace Gibbed.DeusEx3.FileFormats
+{
+    public class BigFile
+    {
+        public bool LittleEndian;
+        public uint FileAlignment;
+        public string BasePath;
+        public List<Big.Entry> Entries
+            = new List<Big.Entry>();
+
+        public int EstimateHeaderSize()
+        {
+            return
+                4 + // file alignment
+                64 + // base path
+                4 + // count
+                (4 * this.Entries.Count) + // name hashes
+                (16 * this.Entries.Count) // entry table
+                .Align(2048); // aligned to 2048 bytes
+        }
+
+        public void Serialize(Stream output)
+        {
+            output.WriteValueU32(this.FileAlignment, this.LittleEndian);
+            output.WriteString(this.BasePath, 64, Encoding.ASCII);
+            output.WriteValueS32(this.Entries.Count, this.LittleEndian);
+
+            var entries = this.Entries.OrderBy(e => e.NameHash);
+            
+            foreach (var entry in entries)
+            {
+                output.WriteValueU32(entry.NameHash, this.LittleEndian);
+            }
+
+            foreach (var entry in entries)
+            {
+                output.WriteValueU32(entry.Size, this.LittleEndian);
+                output.WriteValueU32(entry.Offset, this.LittleEndian);
+                output.WriteValueU32(entry.Locale, this.LittleEndian);
+                output.WriteValueU32(entry.Unknown4, this.LittleEndian);
+            }
+        }
+
+        public void Deserialize(Stream input)
+        {
+            var fileAlignment = input.ReadValueU32(true);
+            if (fileAlignment != 0x7FF00000 &&
+                fileAlignment != 0x0000F07F &&
+                fileAlignment != 0x62300000 &&
+                fileAlignment != 0x00003062)
+            {
+                throw new FormatException("unexpected file alignment (should have been 0x7FF00000)");
+            }
+
+            this.LittleEndian =
+                fileAlignment == 0x7FF00000 ||
+                fileAlignment != 0x62300000;
+            this.FileAlignment = this.LittleEndian == true ?
+                fileAlignment : fileAlignment.Swap();
+
+            this.BasePath = input.ReadString(64, true, Encoding.ASCII);
+
+            var count = input.ReadValueU32(this.LittleEndian);
+            
+            var hashes = new uint[count];
+            for (uint i = 0; i < count; i++)
+            {
+                hashes[i] = input.ReadValueU32(this.LittleEndian);
+            }
+
+            this.Entries.Clear();
+            for (uint i = 0; i < count; i++)
+            {
+                var entry = new Big.Entry();
+                entry.NameHash = hashes[i];
+                entry.Size = input.ReadValueU32(this.LittleEndian);
+                entry.Offset = input.ReadValueU32(this.LittleEndian);
+                entry.Locale = input.ReadValueU32(this.LittleEndian);
+                entry.Unknown4 = input.ReadValueU32(this.LittleEndian);
+                this.Entries.Add(entry);
+
+                if (entry.Unknown4 != 0)
+                {
+                    throw new NotSupportedException();
+                }
+            }
+        }
+    }
+}
---
+++ Gibbed.DeusEx3.FileFormats/FileExtensions.cs
@@ -0,0 +1,109 @@
+﻿/* Copyright (c) 2011 Rick (rick 'at' gibbed 'dot' us)
+ * 
+ * This software is provided 'as-is', without any express or implied
+ * warranty. In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ * 
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ * 
+ * 1. The origin of this software must not be misrepresented; you must not
+ *    claim that you wrote the original software. If you use this software
+ *    in a product, an acknowledgment in the product documentation would
+ *    be appreciated but is not required.
+ * 
+ * 2. Altered source versions must be plainly marked as such, and must not
+ *    be misrepresented as being the original software.
+ * 
+ * 3. This notice may not be removed or altered from any source
+ *    distribution.
+ */
+
+namespace Gibbed.DeusEx3.FileFormats
+{
+    public static class FileExtensions
+    {
+        public static string Detect(byte[] guess, int read)
+        {
+            if (read == 0)
+            {
+                return "null";
+            }
+
+            if (
+                read >= 4 &&
+                guess[0] == 'C' &&
+                guess[1] == 'D' &&
+                guess[2] == 'R' &&
+                guess[3] == 'M')
+            {
+                return "drm";
+            }
+            else if (
+                read >= 4 &&
+                guess[0] == 'C' &&
+                guess[1] == 'R' &&
+                guess[2] == 'I' &&
+                guess[3] == 'D')
+            {
+                return "cpk";
+            }
+            else if (
+                read >= 4 &&
+                guess[0] == 'F' &&
+                guess[1] == 'S' &&
+                guess[2] == 'B' &&
+                guess[3] == '4')
+            {
+                return "sam";
+            }
+            else if (
+                read >= 4 &&
+                guess[0] == 0x44 &&
+                guess[1] == 0xAC &&
+                guess[2] == 0 &&
+                guess[3] == 0)
+            {
+                return "mul";
+            }
+            else if (
+                read >= 4 &&
+                guess[3] == 0x44 &&
+                guess[2] == 0xAC &&
+                guess[1] == 0 &&
+                guess[0] == 0)
+            {
+                return "mul";
+            }
+            else if (
+                read >= 4 &&
+                guess[0] == 0x80 &&
+                guess[1] == 0xBB &&
+                guess[2] == 0 &&
+                guess[3] == 0)
+            {
+                return "mul";
+            }
+            else if (
+                read >= 4 &&
+                guess[3] == 0x80 &&
+                guess[2] == 0xBB &&
+                guess[1] == 0 &&
+                guess[0] == 0)
+            {
+                return "mul";
+            }
+            else if (
+                read >= 4 &&
+                guess[0] == 'M' &&
+                guess[1] == 'u' &&
+                guess[2] == 's')
+            {
+                return "mus";
+            }
+
+            return "unknown";
+        }
+    }
+}
